31bea4e2a66112dde21f8e8d770aa1d8a41569a2
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-06 00:18:02 +0530

    update

--- my_react/src/index.js
+++ my_react/src/index.js
@@ -1,17 +1,6 @@
+// const React = require('react');
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom';
+// const ReactDOM = require('react-dom')
+//this is converted into create.Reactelement by babbel
+ReactDOM.render(<h1>This is the first application</h1>,document.getElementById("root"));
