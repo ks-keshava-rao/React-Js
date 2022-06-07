5c17a9f509ada524f4171b67327ab0a8a28ec336
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-07 18:50:53 +0530

    Create List.jsx

---
+++ my_react/src/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function List(){
+    return (
+        <ol>
+            <li>Vanilla JS</li>
+            <li>Node JS</li>
+            <li>React JS</li>
+            <li>Express JS</li>
+        </ol>
+    )
+}
+export default List;
