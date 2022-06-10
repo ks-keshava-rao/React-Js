70791a44e053907e1a28270c210ff1acac568229
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-10 18:33:19 +0530

    Update App.jsx

--- my_react/src/App.jsx
+++ my_react/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
 import List from "./List";
+import SubHeader from "./SubHeader";
 
 function App() {
     return (
         <>
             <Heading />
+            <SubHeader/>
             <Paragraph />
             <List />
         </>
