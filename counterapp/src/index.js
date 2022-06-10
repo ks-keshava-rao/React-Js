0555b388318153516d5b8d8ca61a2cba065c6b84
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-10 18:33:17 +0530

    Create index.js

---
+++ counterapp/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Counter from "./Counter.jsx";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <>
+    <Counter/>;
+    </>,document.getElementById('root')
+  )
