717407703142e9f49640554d0cc664d57c155205
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-10 18:33:21 +0530

    Create SubHeader.jsx

---
+++ my_react/src/SubHeader.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+class SubHeader extends Component {
+    render(){
+        return(
+            <h1>This is rendered from class component</h1>
+        )
+    }
+}
+export default SubHeader;
