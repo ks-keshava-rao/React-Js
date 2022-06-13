012d768256810f45b8967213e0429077c5ac7d33
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-13 20:24:14 +0530

    Create HookEffect.jsx

---
+++ my_react/src/HookEffect.jsx
@@ -0,0 +1,4 @@
+// Performs side effects in functional components like data fetching 
+// setting up a subscription and manual changing of DOM , api call
+// Side effects are all the operations that affect your component and can't be done during rendering.
+//combination of componentDidMount , componentDidUpdate, and ComponentWillUnmount
