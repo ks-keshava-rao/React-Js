b0e3905e04d9fa09c25f05dc1ed24916f4c0f1a8
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-15 01:09:33 +0530

    Update App.jsx

--- my_react/src/App.jsx
+++ my_react/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import Heading from "./Heading";
-import Paragraph from "./Paragraph";
-import List from "./List";
-import SubHeader from "./SubHeader";
-
+// import Heading from "./Heading";
+// import Paragraph from "./Paragraph";
+// import List from "./List";
+// import SubHeader from "./SubHeader";
+// import HookEffect from "./HookEffect";
+// import HookReduce from "./HookReduce";
+// import Coordinate from "./Coordinate";
+import HookCallback from "./HookCallback";
 function App() {
     return (
         <>
-            <Heading />
+            {/* <Heading />
             <SubHeader/>
             <Paragraph />
-            <List />
+            <List /> */}
+            {/* <HookEffect/> */}
+            {/* <HookReduce/>
+            <Coordinate/> */}
+            <HookCallback/>
+          
         </>
     )
 }
