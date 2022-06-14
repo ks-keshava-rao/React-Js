0671937a61ff4f42112137ce20fdbaba3676d66e
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-15 01:09:29 +0530

    Create Coordinate.js

---
+++ my_react/src/Coordinate.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function Coordinate(){
+    const [x,setX] = useState(0)
+    const [y,setY] = useState(0);
+
+    const getMousePosition = (e) =>{
+        console.log('Mouse event')
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+    useEffect(()=>{
+        console.log("use effect called");
+        window.addEventListener('mousemove',getMousePosition)
+    },[])
+    return (
+        <>
+        Position X - {x} 
+        Position Y - {y}
+        </>
+    )
+}
+export default Coordinate
