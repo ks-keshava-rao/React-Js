70d48248213a968b012a70b5676e3e8d4b4fa9bd
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-20 23:38:30 +0530

    Update App.js

--- student_portal/src/App.js
+++ student_portal/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
+import LoginAdmin from './components/LoginAdmin';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/Login' element={<Login/>}/>
+        <Route exact path='/Signup' element={<Signup/>}/>
+        <Route exact path='/admin_login' element={<LoginAdmin/>}/>
+      </Routes>
+    </Router>
   );
 }
 
