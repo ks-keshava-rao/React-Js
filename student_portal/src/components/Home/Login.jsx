212e4a37a7025d2961a8b26635b12687a2d86509
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-23 21:30:15 +0530

    Update Login.jsx

--- student_portal/src/components/Home/Login.jsx
+++ student_portal/src/components/Home/Login.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-
+import React,{useEffect,useState} from 'react'
 const Login = () => {
-    return (
+    const initialvalue = {
+        idNumber : '',
+        password : ''
+    }
+    const [logindata,setLoginData] = useState(initialvalue)
+    const HandleLoginInputs = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        // console.log(name,value);
+        setLoginData({...logindata,[name]:value});
+    } 
+    const HandleLogin = (event) => {
+        event.preventDefault()
+        const targetButton = event.target.name
+        console.log(targetButton); 
+    }
+    return ( 
         <div className="container-sm">
             <br /> <br />
-            <form>
+            <form  name='loginform'>
                 <div className="form-group py-2">
-                    <label >Roll Number</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Roll number" required/>
+                    <label >ID no.</label>
+                    <input type="text" name='idNumber'onChange={HandleLoginInputs} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Roll number" required/>
                 </div>
                 <div className="form-group py-2">
                     <label >Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required/>
+                    <input type="password" name='password' onChange={HandleLoginInputs} className="form-control" id="exampleInputPassword1" placeholder="Password" required/>
                 </div> <br />
                 <div className="container">
-                <button type="submit" className="btn btn-primary m-4">Login as Student</button> 
-                <button type="submit" className="btn btn-primary">Login as Admin</button>
+                <button  name='studentsubmit' onClick={HandleLogin}  id='studentbtn'className="btn btn-primary m-4">Login as Student</button> 
+                <button  name='adminsubmit' onClick={HandleLogin}  id='adminbtn' className="btn btn-primary">Login as Admin</button>
                 </div>
             </form>
         </div>
