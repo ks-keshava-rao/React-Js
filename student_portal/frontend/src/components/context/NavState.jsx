8d0ac5069ff370098f5ea9b4794a876a14a74722
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-06-28 23:36:21 +0530

    Create NavState.jsx

---
+++ student_portal/frontend/src/components/context/NavState.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+import NavbarContext from "./NavContext";
+
+const NavbarState = (props) =>{
+    const prelogin = [
+        {
+            pageName: "Home",
+            path: '/'
+          },
+          {
+            pageName: "Login",
+            path: '/login'
+          },
+          {
+            pageName: "sample",
+            path: '/sample'
+          }
+        ]
+    
+    const [navbarchoice,updatechoice] = useState(prelogin);
+    return(
+        <NavbarContext.Provider value={{navbarchoice}}>
+            {console.log(navbarchoice)}
+            {props.children}
+        </NavbarContext.Provider>
+    )
+}
+export default NavbarState
