262938f6a118929e0fa3bb029359648e9426abb2
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-07-01 23:55:40 +0530

    Update Home.jsx

--- student_portal/frontend/src/components/Home/Home.jsx
+++ student_portal/frontend/src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Studentcontext from '../context/studentContext';
+import { useContext } from 'react';
 function Home(props) {
+  const {studentdetails,updatestudentdetails} = useContext(Studentcontext);
+  // console.log(studentdetails);
   return (
     <div className='container'>
             <br /> <br />
             <br /> <br />
-        <h1>Welcome to Portal {props.studentname} </h1>
+        <h1>Welcome to Portal  </h1>
     </div>
   )
 }
