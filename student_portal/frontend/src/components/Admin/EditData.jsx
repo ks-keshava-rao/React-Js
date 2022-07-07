1eeaaa6183c065426cdb3b2110497befd185dd30
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-07-07 23:45:18 +0530

    Update EditData.jsx

--- student_portal/frontend/src/components/Admin/EditData.jsx
+++ student_portal/frontend/src/components/Admin/EditData.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
 const EditData = () => {
+  const [users, setUser] = useState([])
+  useEffect(() => {
+    loadUsers();
+  }, []);
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/usersdata")
+    // console.log(result.data);
+    setUser(result.data)
+  }
+  console.log(users)
   return (
-    <div>EditData</div>
+    <>
+      <div className='container'>
+        <div className='py-4'>
+          <table className="table border shadow">
+            <thead className='thead-dark'>
+              <tr>
+                <th scope='col'>Sno.</th>
+                <th scope='col'>Roll Number</th>
+                <th scope='col'>Student Name</th>
+                <th scope='col'>Email-id</th>
+                <th scope='col'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                users.map((user, index) => 
+                  (
+                  <tr key={index}>
+                    <td scope='row'>{index+1}</td>
+                    <td>{user.rollNumber}</td>
+                    <td>{user.studentName}</td>
+                    <td>{user.Usermail}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   )
 }
 
