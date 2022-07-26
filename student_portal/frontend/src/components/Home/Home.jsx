a8d8af682cdc38c64cc9cfbacd2433724e839d56
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-07-26 20:38:02 +0530

    Update Home.jsx

--- student_portal/frontend/src/components/Home/Home.jsx
+++ student_portal/frontend/src/components/Home/Home.jsx
@@ -3,19 +3,20 @@ import Studentcontext from '../context/studentContext';
 import { useContext } from 'react';
 import DisplayNamecontext from '../context/DisplayNamecontext';
 function Home(props) {
-  const {studentdetails,updatestudentdetails} = useContext(Studentcontext);
-  const {displayname} = useContext(DisplayNamecontext);
+  const { studentdetails, updatestudentdetails } = useContext(Studentcontext);
+  const { displayname } = useContext(DisplayNamecontext);
   console.log(displayname);
   return (
     <>
-    <div>
-    <div className='container'>
-            <br /> <br />
-            <br /> <br />
-            
-        <h1 className=' display-4 text-center mt-9'>  Welcome to Portal {displayname} </h1>
-    </div>
-    </div>
+    <div className='pt-5 mt-5'>
+      <div className="w-75 mx-auto  border rounded shadow  p-5">
+        <div className='container'>
+          <br /> <br />
+          <br /> <br />
+          <h1 className=' display-4 text-center mb-5 pb-5'>  Welcome to Portal {displayname} </h1>
+        </div>
+      </div>
+      </div>
     </>
   )
 }
