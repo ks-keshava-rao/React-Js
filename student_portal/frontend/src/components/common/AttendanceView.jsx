51a128b15cda9acfd8042c2869ddd09ef972d31f
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-08-02 21:49:42 +0530

    Update AttendanceView.jsx

--- student_portal/frontend/src/components/common/AttendanceView.jsx
+++ student_portal/frontend/src/components/common/AttendanceView.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin,{DateClickArg} from '@fullcalendar/interaction'
+import { useState } from 'react';
 const AttendanceView = () => {
+  const [CalEvent , setEvent] = useState([]);
   const handleDateClick = (currDate)=>{
-   if(currDate.jsEvent.altKey) console.log(currDate);
+   if(currDate.jsEvent.altKey) {
+   console.log(currDate.dateStr);
+   setEvent([
+    { title: 'event 1', date: currDate.date }
+  ])
+  console.log(CalEvent)
+   }
 
   }
   return (
     <div className="container-sm">
     <FullCalendar 
+     events={CalEvent}
     plugins={[dayGridPlugin,interactionPlugin]}
     dateClick={(e)=>{handleDateClick(e)}}
     />
